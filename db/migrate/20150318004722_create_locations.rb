d537f1cb1258e97412de6c746dcd17958254efa3
Rhoxio <rhoxiodbc@gmail.com>
2015-03-17 21:26:26 -0700

    Added the item migration. Works and does not interfere.

--- db/migrate/20150318004722_create_locations.rb
+++ db/migrate/20150318004722_create_locations.rb
@@ -2,8 +2,12 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :name
+      t.string :background_path
       t.string :coordinates, :array => true
-      t.integer :enemy_types
+
+      t.integer :enemy_types, :array => true
+      t.integer :npc_types, :array => true
+      t.integer :shop_types, :array => true
 
       t.timestamps null: false
     end
