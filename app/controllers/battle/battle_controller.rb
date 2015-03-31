cf66f5c01fd63d34ddf1f546bee8bed21083181a
Rhoxio <rhoxiodbc@gmail.com>
2015-03-30 20:42:00 -0700

    Added a few annotations for myself for next time.

--- app/controllers/battle/battle_controller.rb
+++ app/controllers/battle/battle_controller.rb
@@ -1,6 +1,11 @@
 module Battle
 	class BattleController < ApplicationController
 
+		# You still need to make this a helper method.
+		# If the server currently has the socket 'open', then
+		# do not attempt to open the socket again and simply
+		# render the page. 
+
 		def open_socket
 			EventMachine.run{
 				EventMachine::WebSocket.start(:host => 'localhost', :port => 9001) do |ws|
