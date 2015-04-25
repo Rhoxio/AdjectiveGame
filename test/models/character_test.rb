0c13fb2568edf0714692e5561d13acce9677fd6d
Rhoxio <rhoxiodbc@gmail.com>
2015-04-24 21:55:46 -0700

    Added descriptions to the character model tests. Should he me debug down the road if something messes up.

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -8,46 +8,42 @@ class CharacterTest < ActiveSupport::TestCase
 	debuff = Status.create!(name: 'Poison', damage: 1, duration: 3, status_type: 'damage')
 	buff = Status.create!(name: 'Renew', healing: 1, duration: 3, status_type: 'healing')
 
-  test "that this shit is even loading correctly" do
-    assert azorius
-  end
-
   test 'characters take damage' do
   	before_hit = azorius.hitpoints
   	azorius.take_damage(1)
 
-  	assert azorius.hitpoints < before_hit
+  	assert(azorius.hitpoints < before_hit, 'Character did not take damage.')
   end
 
   test 'weapons may associate with characters' do
   	weapon = Weapon.create!(name: 'Zweihander')
 
   	azorius.weapon = weapon
-  	assert azorius.weapon
+  	assert(azorius.weapon, 'Character model does not associate weapons with itself')
   end
 
   test 'characters do not go over max hp when hp is restored' do
   	azorius.restore_hitpoints(1000)
-  	assert azorius.hitpoints == azorius.max_hitpoints
+  	assert(azorius.hitpoints == azorius.max_hitpoints, 'Characters hp did not stop at max, or the object used has over 1000 hp')
   end
 
   test 'character can become debuffed' do
   	debuff = Status.create!(name: 'Poison', damage: 1, duration: 3)
   	azorius.add_debuff(debuff)
-  	assert(azorius.debuffs['Poison'], '')
+  	assert(azorius.debuffs['Poison'], 'Debuff did not apply')
   end
 
   test 'character can become buffed' do
   	azorius.add_buff(buff)
-  	assert(azorius.buffs['Renew'], '')
+  	assert(azorius.buffs['Renew'], 'Buff did not apply')
   end
 
   test 'character can mitigate all damage' do
-  	assert azorius.full_mitigation_check(100)
+  	assert(azorius.full_mitigation_check(100), 'Character did not mitigate damge at 100% chance.')
   end
 
   test 'character can be hit when mitigation check fails' do
-  	assert azorius.full_mitigation_check(0) == false
+  	refute(azorius.full_mitigation_check(0), 'Move still resolved to true.')
   end
 
   test 'character damage debuffs tick as expected' do
@@ -55,7 +51,7 @@ class CharacterTest < ActiveSupport::TestCase
 
   	azorius.add_debuff(debuff)
   	azorius.tick_all_debuffs
-  	assert azorius.hitpoints < health_before
+  	assert(azorius.hitpoints < health_before, "Damage debuffs didn't tick correctly")
   end
 
   test 'character healing buffs tick as expected' do
@@ -72,21 +68,27 @@ class CharacterTest < ActiveSupport::TestCase
   	azorius.add_buff(buff)
   	azorius.clear_statuses
 
-  	assert(azorius.debuffs == {} && azorius.buffs == {})
+  	assert(azorius.debuffs == {} && azorius.buffs == {}, 'Debuffs did not clear correctly.')
   end
 
   test 'should acquire gold' do
   	azorius.acquire_gold(10)
-  	assert azorius.gold == 10
+  	assert(azorius.gold == 10, 'Character did not acquire gold')
   end
 
   test 'should give gold if character has enough' do
   	rhoxio.acquire_gold(10)
-  	assert(rhoxio.gold == 10, '')
+  	assert(rhoxio.gold == 10, 'Character did not give up gold')
   end
 
-  # test '' do
+  test 'character should be alive with more than 1 hp' do
+  	azorius.hitpoints = 1
+  	assert(azorius.alive?, 'Character is not alive even though their hp is over 0.')
+  end
 
-  # end
+  test 'character should be dead with 0 or less than 0 hp.' do
+  	azorius.hitpoints = 0
+  	refute(azorius.alive?, 'Character did not register as dead even though hp was 0.')
+  end
 
 end
